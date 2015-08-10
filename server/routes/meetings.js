09cede3c6f6dfd417371d572e0a882fe2dafc348
Sebastin M. Perez <perezsebastianm@gmail.com>
2015-08-09 21:22:32 -0300

    Add Users resource

--- server/routes/meetings.js
+++ server/routes/meetings.js
@@ -1,24 +1,29 @@
 var meetings = {
+
     getAll: function(req, res) {
         var allMeetings = data; // Spoof a DB call
         res.json(allMeetings);
     },
+
     getOne: function(req, res) {
         var id = req.params.id;
         var meeting = data[0]; // Spoof a DB call
         res.json(meeting);
     },
+
     create: function(req, res) {
         var newMeeting = req.body;
         data.push(newMeeting); // Spoof a DB call
         res.json(newMeeting);
     },
+
     update: function(req, res) {
         var updateMeeting = req.body;
         var id = req.params.id;
         data[id] = updateMeeting // Spoof a DB call
         res.json(updateMeeting);
     },
+
     delete: function(req, res) {
         var id = req.params.id;
         data.splice(id, 1) // Spoof a DB call
@@ -26,15 +31,19 @@ var meetings = {
     }
 };
 
-var data = [{
-    name: 'Meeting 1',
-    id: '1'
-}, {
-    name: 'Meeting 2',
-    id: '2'
-}, {
-    name: 'Meeting 3',
-    id: '3'
-}];
+var data = [
+    {
+        id: '1'
+        name: 'Meeting 1',
+    }, 
+    {
+        id: '2'
+        name: 'Meeting 2',
+    }, 
+    {
+        id: '3'
+        name: 'Meeting 3',
+    }
+];
 
 module.exports = meetings;
---
+++ server/routes/users.js
@@ -0,0 +1,49 @@
+var users = {
+
+    getAll: function(req, res) {
+        var allUsers = data; // Spoof a DB call
+        res.json(allUsers);
+    },
+
+    getOne: function(req, res) {
+        var id = req.params.id;
+        var user = data[0]; // Spoof a DB call
+        res.json(user);
+    },
+
+    create: function(req, res) {
+        var newUser = req.body;
+        data.push(newUser); // Spoof a DB call
+        res.json(newUser);
+    },
+
+    update: function(req, res) {
+        var updateUser = req.body;
+        var id = req.params.id;
+        data[id] = updateUser // Spoof a DB call
+        res.json(updateUser);
+    },
+
+    delete: function(req, res) {
+        var id = req.params.id;
+        data.splice(id, 1) // Spoof a DB call
+        res.json(true);
+    }
+};
+
+var data = [
+    {
+        id: '1'
+        name: 'user 1',
+    }, 
+    {
+        id: '2'
+        name: 'user 2',
+    }, 
+    {
+        id: '3'
+        name: 'user 3',
+    }
+];
+
+module.exports = users;
