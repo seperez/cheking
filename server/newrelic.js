ef495e2a9270f4f4111ecf1341602b7f6776508f
Sebastin M. Perez <perezsebastianm@gmail.com>
2015-08-14 02:46:29 -0300

    Add integration with new relic

---
+++ server/newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Cheking'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'a48400be479225412fda09f29d93f52099d022d1',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
